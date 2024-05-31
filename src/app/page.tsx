d031847e63e46b6ae9399d6470218cde63d6598f
Ussama Zubair <malikusama124@gmail.com>
2024-05-31 15:14:29 +0500

    card component for dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+import { Card } from "@/components/Card";
+
 export default function Home() {
   return (
     <div className="main-content flex flex-col flex-grow p-4">
-      <h1 className="font-bold text-2xl text-customBlue1">Dashboard</h1>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="">
+            <Card title="Total Conversations">
+              <h2 className="color-blue mt-2 pb-0">1890.92</h2>
+            </Card>
+          </div>
+          <div className="">
+            <Card title="Average time of conversation">
+              <h2 className="color-blue mt-2 pb-0">1890.92</h2>
+            </Card>
+          </div>
+          <div className="">
+            <Card title="New Leads">
+              <h2 className="color-blue mt-2 pb-0">1890.92</h2>
+            </Card>
+          </div>
+        </div>
+      </div>
 
-      <div className="flex flex-col flex-grow border-4 border-gray-400 border-dashed bg-white rounded mt-4"></div>
+      <div className="mt-4">
+        <Card
+          title="Conversations / Date"
+          desc="Data is sorted based on the date when the conversations were created."
+        >
+          <div className="h-[300px] border-4 border-gray-400 border-dashed bg-white rounded mt-3"></div>
+        </Card>
+      </div>
+
+      <div className="mt-4">
+        <Card
+          title="Conversations / Time"
+          desc="Data is sorted based on the time when the conversations were created."
+        >
+          <div className="h-[300px] border-4 border-gray-400 border-dashed bg-white rounded mt-3"></div>
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface Props {
+  title: string;
+  desc?: string;
+  children: React.ReactNode;
+}
+
+export const Card: React.FC<Props> = ({ title, desc, children }) => {
+  return (
+    <div className="flex flex-col border-1 radius-1 w-full bg-white p-4">
+      <div className="font-primary mb-1">{title}</div>
+      <div className="font-secondary mb-1">{desc}</div>
+      {children}
+    </div>
+  );
+};
