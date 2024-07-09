64b5f5394f341ba7fc4a26ed8b47eaecfcc76e40
Ussama Zubair <malikusama124@gmail.com>
2024-07-09 20:07:51 +0500

    paginated sources

--- src/app/sources/page.tsx
+++ src/app/sources/page.tsx
@@ -11,7 +11,7 @@ import { useEffect, useState } from "react";
 export default function Sources() {
   const { loading, error, makeRequest } = useApiRequest();
   const [search, setSearch] = useState("");
-  const [sources, setSources] = useState<any>([]);
+  const [sources, setSources] = useState<any>({});
   const router = useRouter();
   const { notify } = useNotification();
   const columns = ["filename", "size", "created_at"];
@@ -26,15 +26,22 @@ export default function Sources() {
     fetchSources();
   }, []);
 
-  const fetchSources = async () => {
-    const result: any = await makeRequest("/document", "GET");
+  const fetchSources = async (page = 0) => {
+    const result: any = await makeRequest(
+      `/document?page=${page || 1}&size=${15}`,
+      "GET"
+    );
     result.data.forEach((s: any) => (s.size = formatFileSize(s.size)));
 
-    setSources(result.data);
+    setSources(result);
 
     if (!result.data.length) {
       router.push("/sources/create");
     }
+  };
+
+  const pageChange = (page: number) => {
+    fetchSources(page);
   };
 
   const handleDelete = async (item: any) => {
@@ -48,35 +55,42 @@ export default function Sources() {
       {error ? (
         <div>{error}</div>
       ) : (
-        <>
-          <div className="flex justify-between items-center mb-4">
-            <input
-              type="text"
-              placeholder="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="w-[300px] h-[40px] text-gray-700 rounded-full input-text px-4 py-2"
-            />
-            <button
-              className="w-[200px] h-[40px] text-center text-white rounded-full bg-orange"
-              type="button"
-              onClick={() => router.push("/sources/create")}
-            >
-              <h3>Add sources</h3>
-            </button>
-          </div>
+        sources?.data?.length && (
+          <>
+            <div className="flex justify-between items-center mb-4">
+              <input
+                type="text"
+                placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-[300px] h-[40px] text-gray-700 rounded-full input-text px-4 py-2"
+              />
+              <button
+                className="w-[200px] h-[40px] text-center text-white rounded-full bg-orange"
+                type="button"
+                onClick={() => router.push("/sources/create")}
+              >
+                <h3>Add sources</h3>
+              </button>
+            </div>
 
-          <Table
-            data={sources}
-            columns={columns}
-            colMap={colMap}
-            search={search}
-            showCheckbox={true}
-            actionsCol={["delete"]}
-            sortableColumns={sortableColumns}
-            onDelete={handleDelete}
-          />
-        </>
+            <Table
+              data={sources.data}
+              columns={columns}
+              colMap={colMap}
+              search={search}
+              showCheckbox={true}
+              actionsCol={["delete"]}
+              sortableColumns={sortableColumns}
+              onDelete={handleDelete}
+              paginated={true}
+              page={sources.page}
+              count={sources.count}
+              size={sources.size}
+              onPageChange={pageChange}
+            />
+          </>
+        )
       )}
     </div>
   );
