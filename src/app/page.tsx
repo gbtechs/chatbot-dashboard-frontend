dab98e67e08ffb7c32fa488b03a9330e167160ef
Ussama Zubair <malikusama124@gmail.com>
2024-08-08 13:35:58 +0500

    rechart instead of chartjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ import {
 import useApiRequest from "@/hooks/useApiRequest";
 import { useEffect, useState } from "react";
 import { Dropdown } from "@/components/Dropdown";
-import { formatChartData, formatSeconds } from "@/utils";
+import { formatChartData, formatDataForRecharts, formatSeconds } from "@/utils";
+import CustomLineChart from "@/components/CustomLineChart";
 
 // Register ChartJS components using ChartJS.register
 ChartJS.register(
@@ -50,14 +51,16 @@ export default function Home() {
       "GET"
     );
 
-    const data = formatChartData(res, "#FB8500");
+    // const data = formatChartData(res, "#FB8500");
+    const data = formatDataForRecharts(res);
     setChartData1(data);
   };
 
   const fetchChartData2 = async () => {
     const res: any = await makeRequest(`/conversations_by_hour`, "GET");
 
-    const data = formatChartData(res, "#219EBC");
+    // const data = formatChartData(res, "#219EBC");
+    const data = formatDataForRecharts(res);
     setChartData2(data);
   };
 
@@ -101,8 +104,11 @@ export default function Home() {
           title="Conversations / Date"
           desc="Data is sorted based on the date when the conversations were created."
         >
-          <div className="w-full aspect-w-2 aspect-h-1 bg-white rounded mt-3">
-            {chartData1 && <Line data={chartData1} />}
+          <div className="w-full bg-white rounded mt-3">
+            {/* {chartData1 && <Line data={chartData1} />} */}
+            {chartData1 && (
+              <CustomLineChart data={chartData1} stroke="#FB8500" />
+            )}
           </div>
         </Card>
       </div>
@@ -112,8 +118,11 @@ export default function Home() {
           title="Conversations / Time"
           desc="Data is sorted based on the time when the conversations were created."
         >
-          <div className="w-full aspect-w-2 aspect-h-1 bg-white rounded mt-3">
-            {chartData2 && <Line data={chartData2} />}
+          <div className="w-full bg-white rounded mt-3">
+            {/* {chartData2 && <Line data={chartData2} />} */}
+            {chartData2 && (
+              <CustomLineChart data={chartData2} stroke="#219EBC" />
+            )}
           </div>
         </Card>
       </div>
--- src/components/CustomLineChart.tsx
+++ src/components/CustomLineChart.tsx
@@ -1,52 +1,51 @@
-// // components/LineChart.tsx
-// import React from "react";
-// import {
-//   LineChart,
-//   Line,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-//   ResponsiveContainer,
-// } from "recharts";
+// components/LineChart.tsx
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-// interface DataPoint {
-//   name: string;
-//   value: number;
-// }
+interface DataPoint {
+  key: string;
+  value: number;
+}
 
-// interface LineChartProps {
-//   data: DataPoint[];
-//   stroke: string;
-// }
+interface LineChartProps {
+  data: DataPoint[];
+  stroke: string;
+}
 
-// const CustomLineChart: React.FC<LineChartProps> = ({ data, stroke }) => {
-//   return (
-//     <ResponsiveContainer width="100%" height={400}>
-//       <LineChart
-//         data={data}
-//         margin={{
-//           top: 10,
-//           right: 30,
-//           left: 0,
-//           bottom: 0,
-//         }}
-//       >
-//         <CartesianGrid strokeDasharray="3 3" />
-//         <XAxis dataKey="date" />
-//         <YAxis />
-//         <Tooltip />
-//         <Line
-//           type="monotone"
-//           dataKey="value"
-//           stroke={stroke}
-//           strokeWidth="2"
-//           dot={false}
-//         />
-//       </LineChart>
-//     </ResponsiveContainer>
-//   );
-// };
+const CustomLineChart: React.FC<LineChartProps> = ({ data, stroke }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="key" />
+        <YAxis width={30} />
+        <Tooltip />
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke={stroke}
+          strokeWidth="2"
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
-// export default CustomLineChart;
+export default CustomLineChart;
